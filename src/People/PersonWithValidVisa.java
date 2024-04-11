b52d542739b0d972944f141661b8d7679f8c4190
David <David@LAPTOP-9730KJFV.zyxel.com>
2024-04-11 22:16:13 +0200

    Commit

--- src/People/PersonWithValidVisa.java
+++ src/People/PersonWithValidVisa.java
@@ -1,12 +1,40 @@
 package People;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 import Core.Database;
 import Core.DatabaseOfWantedPeople;
-import Core.Visa;
 
 public class PersonWithValidVisa extends Person implements Serializable{
+	
+	public class Visa {
+	    private String visaType;
+	    private LocalDate visaIssuedDate;
+	    private LocalDate visaExpirationDate;
+	    private String visaNumber;
+	    private String visaIssuer;
+
+	    public Visa(String visaType, LocalDate visaIssuedDate, LocalDate visaExpirationDate, String visaNumber, String visaIssuer) {
+	        this.visaType = visaType;
+	        this.visaIssuedDate = visaIssuedDate;
+	        this.visaExpirationDate = visaExpirationDate;
+	        this.visaNumber = visaNumber;
+	        this.visaIssuer = visaIssuer;
+	    }
+
+	    public String displayInfo() {
+	        
+	    	StringBuilder builder = new StringBuilder();
+		    builder.append("Type of visa: ").append(visaType).append("\n");
+		    builder.append("Issued date of visa: ").append(visaIssuedDate).append("\n");
+		    builder.append("Expiration date of visa: ").append(visaExpirationDate).append("\n");
+		    builder.append("Visa number: ").append(visaNumber).append("\n");
+		    builder.append("Visa issuer: ").append(visaIssuer).append("\n").append("\n");
+		    return builder.toString(); 
+	    }
+	}
+	
     private Visa visa; 
 
     public PersonWithValidVisa() {
@@ -16,8 +44,6 @@ public class PersonWithValidVisa extends Person implements Serializable{
 
     @Override
     public String displayInfo() {
-        /*super.displayInfo();
-        visa.displayInfo(); */
     	
     	StringBuilder builder=new StringBuilder();
     	builder.append(super.displayInfo());
